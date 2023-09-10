efd78812106300db19c75894993e6644e98a481f
Hustra03 <erik.paulinder@gmail.com>
2023-09-10 10:32:32 +0200

    modified:   bin/App.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,35 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int arrayToBeSorted[] ={2,1,3,4,5,6,7,8,9};
+        int arrayToBeSorted[] = { 2, 1 ,3,2,1};
 
         benchmarkSort(arrayToBeSorted);
 
+        //printIntArray(swap(arrayToBeSorted, 0, 1));
+
     }
 
-    static public void benchmarkSort(int[] arrayToBeSorted)
-    {
+    static public void benchmarkSort(int[] arrayToBeSorted) {
         System.out.print("Original array: ");
         printIntArray(arrayToBeSorted);
-        int arrayCopy[]= arrayToBeSorted;
 
-        arrayCopy=selectionSort(arrayCopy);
+        System.out.print("Selection Sort: ");
+        printIntArray(selectionSort(arrayToBeSorted));
 
-        arrayToBeSorted=insertionSort(arrayToBeSorted);
-
-        System.out.println("Selection Sort: ");
-        printIntArray(arrayCopy);
-
-        System.out.println("Insertion Sort: ");
-        printIntArray(arrayToBeSorted);
-
+        System.out.print("Insertion Sort: ");
+        printIntArray(insertionSort(arrayToBeSorted));
 
     }
 
-    static public void printIntArray(int[] arrayToBePrinted)
-    {
+    static public void printIntArray(int[] arrayToBePrinted) {
         for (int i = 0; i < arrayToBePrinted.length; i++) {
-            System.out.print(arrayToBePrinted[i]+",");
+            System.out.print(arrayToBePrinted[i] + ",");
         }
         System.out.println("");
     }
@@ -38,36 +32,33 @@ public class App {
         for (int i = 0; i < array.length - 1; i++) {
             int candidate = i;
             for (int j = i; j < array.length; j++) {
-                if (array[j] < array[i]) {
+                if (array[j] < array[candidate]) {
                     candidate = j;
                 }
+
             }
-            if (candidate!=i) {
-                 array=swap(array,array[i],array[candidate]);
+            if (candidate != i) {
+                array =swap(array, i, candidate);
             }
-           
-            
         }
         return array;
     }
 
     static public int[] insertionSort(int[] array) {
 
-            for (int i = 0; i < array.length; i++) {
-                for (int j = i; j > 0 && array[j]<array[j-1] ; j--) {
-                array=swap(array, array[j-1],array[j]);
-                }
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i; j > 0 && array[j] < array[j - 1]; j--) {
+                array = swap(array, j - 1, j);
             }
-            return array;
         }
-
-    static public int[] swap(int[] array, int firstIndex, int secondIndex)
-    {
-            int candidate = array[firstIndex];
-            array[firstIndex] = array[secondIndex];
-            array[secondIndex] = candidate;
-            
         return array;
     }
-    
+
+    static public int[] swap(int[] array, int firstIndex, int secondIndex) {
+        int candidate = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = candidate;
+        return array;
+    }
+
 }
