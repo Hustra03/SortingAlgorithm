79a3aeb0bfb75d1b5da2d930300140d928018952
Hustra03 <erik.paulinder@gmail.com>
2023-09-12 15:47:41 +0200

    modified:   bin/App.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,27 +1,80 @@
+import java.util.Random;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
 
-        int arrayToBeSorted[] = { 2, 1, 3, 2, 1 };
+        int numberOfAttempts = 1000;
+        int arraySizes[] = { 100, 200, 500, 1000, 2500, 5000 };
 
-        benchmarkSort(arrayToBeSorted);
+        for (int i : arraySizes) {
+
+            benchmarkSort(i, numberOfAttempts);
+            System.out.println(" ");
+        }
 
     }
 
-    static public void benchmarkSort(int[] arrayToBeSorted) {
-        System.out.print("Original array: ");
-        printIntArray(arrayToBeSorted);
+    static public void benchmarkSort(int arraySize, int numberOfAttempts) {
 
-        System.out.print("Selection Sort: ");
-        printIntArray(selectionSort(arrayToBeSorted));
+        int arrayToBeSorted[] = createUnsortedArray(arraySize);
+        long t0;
+        long t1;
+        long minimum = Long.MAX_VALUE;
 
-        System.out.print("Insertion Sort: ");
-        printIntArray(insertionSort(arrayToBeSorted));
+        for (int i = 0; i < numberOfAttempts; i++) {
 
-        int copyArray[]=arrayToBeSorted;
-        mergeSort(copyArray);
-        System.out.print("Merge Sort: ");
-        printIntArray(copyArray);
+            t0 = System.nanoTime();
+            // printIntArray(selectionSort(arrayToBeSorted));
+            selectionSort(arrayToBeSorted);
+            t1 = System.nanoTime();
+            if (minimum > (t1 - t0)) {
+                minimum = (t1 - t0);
+            }
+        }
 
+        System.out.println("Selection Sort Size " + arraySize + " : " + (minimum) + " ns");
+
+        minimum = Long.MAX_VALUE;
+
+        for (int i = 0; i < numberOfAttempts; i++) {
+
+            arrayToBeSorted = createUnsortedArray(arraySize);
+            t0 = System.nanoTime();
+            insertionSort(arrayToBeSorted);
+            t1 = System.nanoTime();
+            if (minimum > (t1 - t0)) {
+                minimum = (t1 - t0);
+            }
+        }
+        System.out.println("Insertion Sort Size " + arraySize + " Minimum: " + (minimum) + " ns");
+
+        minimum = Long.MAX_VALUE;
+
+        for (int i = 0; i < numberOfAttempts; i++) {
+
+            arrayToBeSorted = createUnsortedArray(arraySize);
+            t0 = System.nanoTime();
+            mergeSort(arrayToBeSorted);
+            t1 = System.nanoTime();
+            if (minimum > (t1 - t0)) {
+                minimum = (t1 - t0);
+            }
+        }
+
+        System.out.println("Merge Sort Size " + arraySize + " : " + (minimum) + " ns");
+    }
+
+    static public int[] createUnsortedArray(int arraySize) {
+        int array[] = new int[arraySize];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rnd.nextInt(10 * arraySize) + 1;
+        }
+
+        return array;
     }
 
     static public void printIntArray(int[] arrayToBePrinted) {
@@ -49,9 +102,9 @@ public class App {
 
     static public int[] insertionSort(int[] array) {
 
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             for (int j = i; j > 0 && array[j] < array[j - 1]; j--) {
-                array = swap(array, j - 1, j);
+                array = swap(array, j, j - 1);
             }
         }
         return array;
@@ -64,7 +117,7 @@ public class App {
         return array;
     }
 
-//Läs på mer om merge sort, är förvirrande hur den ska fungera
+    // Läs på mer om merge sort, är förvirrande hur den ska fungera
     static public void mergeSort(int[] array) {
         if (array.length == 0)
             return;
@@ -81,8 +134,8 @@ public class App {
 
             // sort the items from mid+1 to hi
 
-            sort(org, aux, mid+1, hi);
-            
+            sort(org, aux, mid + 1, hi);
+
             // merge the two sections using the additional array
             merge(org, aux, lo, mid, hi);
         }
@@ -98,32 +151,29 @@ public class App {
         int j = mid + 1; // the index in the second part
         // for all indices from lo to hi
         for (int k = lo; k <= hi; k++) {
-            if (i>mid) {
-                org[k]=aux[j];
+            if (i > mid) {
+                org[k] = aux[j];
                 j++;
             }
             // if i is greater than mid then
             // move the j'th item to the org array, update j
 
-            else if(j>hi)
-            {
-                org[k]=aux[i];
+            else if (j > hi) {
+                org[k] = aux[i];
                 i++;
             }
             // else if j is greater than hi then
             // move the i'th item to the org array, update i
 
-            else if(org[i]<aux[j])
-            {
-                org[k]=aux[i];
+            else if (org[i] < aux[j]) {
+                org[k] = aux[i];
                 i++;
             }
             // else if the i'th item is smaller than the j¨ath item,
             // move the i'th item to the org array, update i
 
-            else
-            {
-                org[k]=aux[j];
+            else {
+                org[k] = aux[j];
                 j++;
             }
             // else
